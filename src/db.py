1b59723b00314467892671f67e0ca77695efe333
absenth <1115138+absenth@users.noreply.github.com>
2023-06-19 21:26:13 -0500

    Refactoring with sqlalchemy

--- src/db.py
+++ src/db.py
@@ -1,67 +1,86 @@
 import os
 import psycopg2
+import sqlalchemy as db
 from dotenv import load_dotenv
 
 
-def openconn():
+def generate_engine():
+    load_dotenv()
     dbname = os.getenv('DBNAME')
     dbhost = os.getenv('DBHOST')
     dbuser = os.getenv('DBUSER')
     dbpass = os.getenv('DBPASS')
     dbport = os.getenv('DBPORT')
-
-    conn = psycopg2.connect(database=dbname,
-                            host=dbhost,
-                            user=dbuser,
-                            password=dbpass,
-                            port=dbport)
-    cursor = conn.cursor()
-    return conn, cursor
+    engine = db.create_engine("postgresql://dbuser:dbpass@dbhost:dbport/dbname")
+    return engine
 
 
-def closeconn(conn, cursor):
-    cursor.close()
-    conn.close()
+def setupdb():
+    engine = generate_engine()
+    metadata = db.MetaData()
+    spots_table = db.Table(
+        "spots",
+        metadata,
+        db.Column("id", db.Integer(), primary_key=True),
+        db.Column("name", db.String),
+        db.Column("website", db.String),
+        db.Column("google_maps_link", db.String),
+    )
+    picked_spots_table = db.MetaData(
+        "picked_spots",
+        metadata,
+        db.Column("id", db.Integer(), primary_key=True),
+        db.Column("spot_id", db.Integer(), db.ForeignKey("spots.id")),
+        db.Column("spot_name", db.String(), db.ForeignKey("spots.name")),
+        db.Column("picked_date", db.DateTime()),
+    )
+
+    metadata.create_all(engine)
 
 
-def setlast(name):
-    conn, cur = openconn()
-    cur.execute('do stuff')
-
-    closeconn(conn, cur)
-    return
-
-
-def getlast():
-    conn, cur = openconn()
-    cur.execute('do stuff')
-
-    closeconn(conn, cur)
-    return name
-
-
-def updatepicks(spot, date):
-    conn, cur = openconn()
-    cur.execute('do stuff')
-
-    closeconn(conn, cur)
-    return
+def get_last_picked_spot():
+    engine = generate_engine()
+    metadata = db.MetaData()
+    picked_spots_table = db.Table(
+        "picked_spots", metadata, autoload=True, autoload_with=engine
+    )
+    query = (
+        db.select([picked_spots_table])
+        .order_by(picked_spots_table.columns.picked_date.desc())
+        .limit(1)
+    )
+    connection = engine.connect()
+    result = connection.execute(query).fetchone()
+    connection.close()
+    return result
 
 
-def getpicks():
-    conn, cur = openconn()
-    cur.execute('do stuff')
-
-    closeconn(conn, cur)
-    return
-
-
-def addspot(name, website, address):
-    conn, cur = openconn()
-    cur.execute('do stuff')
-
-    closeconn(conn, cur)
-    return
+def add_spot(name, website, google_maps_link):
+    engine = generate_engine()
+    metadata = db.MetaData()
+    spots_table = db.Table('spots', metadata, autoload=True, autoload_with=engine)
+    connection = engine.connect()
+    query = spots_table.insert().values(name=name, website=website, google_maps_link=google_maps_link)
+    connection.execute(query)
+    connection.close()
 
 
+def del_spot(spot_id):
+    engine = generate_engine()
+    metadata = db.MetaData()
+    spots_table = db.Table('spots', metadata, autoload=True, autoload_with=engine)
+    connection = engine.connect()
+    query = spots_table.delete().where(spots_table.columns.id == spot_id)
+    connection.execute(query)
+    connection.close()
 
+
+def get_last_five_picks():
+    engine = generate_engine()
+    metadata = db.MetaData()
+    picked_spots_table = db.Table('picked_spots', metadata, autoload=True, autoload_with=engine)
+    query = db.select([picked_spots_table]).order_by(picked_spots_table.columns.picked_date.desc()).limit(5)
+    connection = engine.connect()
+    result = connection.execute(query).fetchall()
+    connection.close()
+    return result
--- src/main.py
+++ src/main.py
@@ -7,45 +7,46 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    token = os.getenv('DISCORD_TOKEN')
+    token = os.getenv("DISCORD_TOKEN")
 
     intents = discord.Intents.default()
     intents.message_content = True
-    bot = commands.Bot(command_prefix='!', intents=intents)
+    bot = commands.Bot(command_prefix="!", intents=intents)
     last = getlast()
 
     @bot.command()
     async def pickspot(ctx):
-        pick,website = pickspot()
+        pick, website = pickspot()
         if pick == last:
-            pick,website = pickspot()
+            pick, website = pickspot()
         else:
-            await ctx.send(f'Picked {pick} as our next spot.  LINK: {website}')
+            await ctx.send(f"Picked {pick} as our next spot.  LINK: {website}")
             setlast(pick)
 
     @bot.command()
     async def getpicks(ctx):
         picks = getpicks()
-        await ctx.sent('These are the next five spots:')
-        await ctx.sent(f'{picks}')
-            
+        await ctx.sent("These are the next five spots:")
+        await ctx.sent(f"{picks}")
 
     bot.run(token)
 
 
 def pickspot():
     locations = {
-            'ABGB': 'https://theabgb.com/',
-            'SABG': 'https://southaustinbeergarden.com/',
-            'Little Woodrows': 'https://littlewoodrows.com/locations/austin-southpark/',
-            "Doc's": "https://www.eatdrinkdocs.com/",
-            'Moontower': 'https://moontowersaloon.com/',
-            'The Little Darlin': 'https://www.thelittledarlin.com/'
-            }
+        "ABGB": "https://theabgb.com/",
+        "SABG": "https://southaustinbeergarden.com/",
+        "Little Woodrows": "https://littlewoodrows.com/locations/austin-southpark/",
+        "Doc's": "https://www.eatdrinkdocs.com/",
+        "Moontower": "https://moontowersaloon.com/",
+        "The Little Darlin": "https://www.thelittledarlin.com/",
+        "Pint House Pizza": "https://www.pinthouse.com/lamar",
+        "The Far Out Lounge": "http://www.thefaroutaustin.com/",
+    }
 
     pick, website = random.choice(list(locations.items()))
-    return pick,website
+    return pick, website
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
